3d610935540bdb4ef55ba8fcc94efd038deab487
Beiting Su <beiting.su@shopee.com>
2021-12-02 14:42:05 +0800

    修改lint检测代码

--- lintrules/src/main/java/com/example/lintrules/ImageViewDetector.kt
+++ lintrules/src/main/java/com/example/lintrules/ImageViewDetector.kt
@@ -2,6 +2,7 @@ package com.example.lintrules
 
 import com.android.tools.lint.detector.api.*
 import org.w3c.dom.Attr
+import org.w3c.dom.Element
 
 /**
  * author: beitingsu
@@ -20,18 +21,25 @@ class ImageViewDetector : Detector(), Detector.XmlScanner {
         )
     }
 
-
-    override fun getApplicableElements(): MutableCollection<String> {
-        return mutableListOf("ImageView")
-    }
+      //这两个方法需要成对使用，但是不能标红
+//    override fun getApplicableElements(): MutableCollection<String> {
+//        return super.getApplicableElements()
+//    }
+//
+//    override fun visitElement(context: XmlContext?, element: Element?) {
+//        super.visitElement(context, element)
+//    }
 
     override fun getApplicableAttributes(): MutableCollection<String> {
         return mutableListOf("src")
     }
 
     override fun visitAttribute(context: XmlContext?, attribute: Attr?) {
-        context?.report(ISSUE, context.getLocation(attribute),
-            "plz use \"app:srcCompat\" instead of \"src\"")
+        //这里需要判断父节点
+        if (attribute?.ownerElement?.tagName == "ImageView") {
+            context?.report(ISSUE, context.getLocation(attribute),
+                "plz use \"app:srcCompat\" instead of \"src\"")
+        }
     }
 
 }
